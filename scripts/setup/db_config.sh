957f0c8aedd6c3a72cddf31a4a379c74b78d43f1
newdoug <sbk7826@g.rit.edu>
2025-06-10 23:22:12 -0400

    Scripts reorganization, postgres DB script

--- scripts/setup/db_config.sh
+++ scripts/setup/db_config.sh
@@ -1,7 +1,8 @@
+# shellcheck disable=SC2034,SC2148
 # This is just an example containing possible fields. You should change these to be your own values and ensure the
 # credentials are safely stored somewhere (not in the repo) and with appropriate ACLs.
 # This file is expected to be sourced by the postgres database setup script. That script will then write these values
 # to a YAML file containing the same information that is meant for the Python code.
-DB_NAME=
+DB_NAME=mlc_data
 DB_USER=user
 DB_PASS=password
--- scripts/setup/postgres.sh
+++ scripts/setup/postgres.sh
@@ -2,33 +2,54 @@
 
 [ "$DEBUG" = "1" ] && set -x
 
+DEFAULT_DB_PORT=5432
+
 if ! command -v psql >/dev/null 2>&1; then
   printf "Please install psql first\\n" >&2
   printf "E.g.: 'apt install -y postgresql'\\n" >&2
   exit 1
 fi
 
-DB_NAME="$1"
-DB_USER="$2"
-DB_PASS="$3"
+DB_CONFIG="$1"
+# shellcheck disable=SC1090
+if [ -z "${DB_CONFIG}" ] || ! source "${DB_CONFIG}" > /dev/null; then
+  printf "Failed to source DB config file '%s'\\n" "${DB_CONFIG}" >&2
+  printf "Usage: %s DB_CONFIG_FILE\\n" "$0" >&2
+  exit 1
+fi
+
+if [ -z "${DB_USER}" ] || [ -z "${DB_PASS}" ] || [ -z "${DB_NAME}" ]; then
+  printf "DB_CONFIG_FILE '%s' must contain definitions for DB_USER and DB_PASS environment variables\\n" "${DB_CONFIG}" >&2
+  exit 1
+fi
+
 if [ -z "${DB_PORT}" ]; then
+  printf "Warning: no DB_PORT in DB_CONFIG_FILE found: default value '%d' will be used\\n" "${DEFAULT_DB_PORT}"
   # The usual default for postgresql
-  DB_PORT=5432
+  DB_PORT="${DEFAULT_DB_PORT}"
 fi
 
 
 SQLFILE="$(mktemp --suffix='.sql')"
+# shellcheck disable=SC2064
 trap "rm -f '$SQLFILE'" EXIT
 
-cat <<'EOF' >"$SQLFILE"
-# Create a user and database
-CREATE USER '${DB_USER}' WITH PASSWORD '${DB_PASS}';
-CREATE DATABASE '${DB_NAME}' OWNER '${DB_USER}';
-GRANT ALL PRIVILEGES ON DATABASE '${DB_NAME}' TO '${DB_USER}';
+cat <<EOF >"$SQLFILE"
+-- Create a user and database
+CREATE USER "${DB_USER}" WITH PASSWORD '${DB_PASS}';
+CREATE DATABASE "${DB_NAME}" OWNER "${DB_USER}";
+GRANT ALL PRIVILEGES ON DATABASE "${DB_NAME}" TO "${DB_USER}";
 EOF
 
-printf "SQLFILE: '%s'\\n" "$SQLFILE"
-cat "$SQLFILE"
+# Make sure postgres user can read it
+sudo chown postgres:postgres "$SQLFILE"
+sudo chmod 700 "$SQLFILE"
 
 # Initial database setup
-sudo su -U postgres psql -U postgres -f "$SQLFILE" "${DB_NAME}"
+sudo -u postgres psql -U postgres -f "$SQLFILE"
+# Truncate before we open permissions to try to delete
+echo | sudo -u postgres tee "$SQLFILE"
+sudo chmod 777 "$SQLFILE"
+# Change back
+sudo chown "$USER:$USER" "$SQLFILE"
+printf "Successfully created DB '%s' and user '%s'. Remember to give these as configuration to any applications that will need to connect to this DB\\n" "${DB_NAME}" "${DB_USER}"
